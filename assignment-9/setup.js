bb82c6de282654e0dd73e25c820a8dda0c304838
EC2 Default User <ec2-user@ip-172-31-12-11.us-east-2.compute.internal>
2018-11-21 17:51:05 +0000

    Add assignment 9

---
+++ assignment-9/setup.js
@@ -0,0 +1,22 @@
+const { Client } = require('pg');
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'rachelsun';
+db_credentials.host = 'dsdemo.c2g7qw1abcde.us-east-1.rds.amazonaws.com';
+db_credentials.database = 'rachelsun';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+// Connect to the AWS RDS Postgres database
+const client = new Client(db_credentials);
+client.connect();
+
+// Sample SQL statement to create a table: 
+var thisQuery = "CREATE TABLE sensorData ( sensorValue boolean, sensorTime timestamp DEFAULT current_timestamp );";
+
+client.query(thisQuery, (err, res) => {
+    console.log(err, res);
+    client.end();
+});
+
