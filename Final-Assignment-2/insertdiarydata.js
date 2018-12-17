f7bccca916b1ff04dac948c905270d52150d1db9
Rachel Sun <rachel.yufei.sun@gmail.com>
2018-12-16 21:22:52 -0500

    Update insertdiarydata.js

--- Final-Assignment-2/insertdiarydata.js
+++ Final-Assignment-2/insertdiarydata.js
@@ -60,3 +60,36 @@ async.eachSeries(diaryEntries, function(value, callback) {
 }, function() {
     console.log('Done!');
 });
+
+// npm install aws-sdk
+var AWS = require('aws-sdk');
+AWS.config = new AWS.Config();
+AWS.config.accessKeyId = process.env.AWS_ID;
+AWS.config.secretAccessKey = process.env.AWS_KEY;
+AWS.config.region = "us-east-1";
+
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+    TableName : "deardiary",
+    KeyConditionExpression: "#tp = :topicName and dt between :minDate and :maxDate", // the query expression
+    ExpressionAttributeNames: { // name substitution, used for reserved words in DynamoDB
+        "#tp" : "topic"
+    },
+    ExpressionAttributeValues: { // the query values
+        ":topicName": {S: "sleep"},
+        ":minDate": {N: new Date("October 7, 2018").valueOf().toString()},
+        ":maxDate": {N: new Date("October 16, 2018").valueOf().toString()}
+    }
+};
+
+dynamodb.query(params, function(err, data) {
+    if (err) {
+        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Query succeeded.");
+        data.Items.forEach(function(item) {
+            console.log("***** ***** ***** ***** ***** \n", item);
+        });
+    }
+});
